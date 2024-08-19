60c26380b6eff72f24dc2443afd96fc8a7744b51
Nsanga Mercure Mekinda <mercuremekinda@gmail.com>
2024-08-19 12:40:04 +0100

    Register done

--- src/form.js
+++ src/form.js
@@ -9,100 +9,73 @@ import {
   Input,
   Button,
   Stack,
-  useToast,
   Text,
   Divider
 } from '@chakra-ui/react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const SignupForm = () => {
   const [formData, setFormData] = useState({
-    nom: '',
-    prenom: '',
+    userId: '',
+    name: '',
+    surname: '',
     email: '',
-    address: '',
+    location: '',
     telephone: '',
     dateOfBirth: '',
     jobTitle: '',
+    language: ''
   });
 
-  const [errors, setErrors] = useState({});
-  const toast = useToast();
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+    // Check if e.target exists to avoid errors
+    if (e.target) {
+      const { name, value } = e.target;
+      setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    }
   };
 
   const handlePhoneChange = (value) => {
-    setFormData({
-      ...formData,
-      telephone: value,
-    });
+    setFormData((prevFormData) => ({ ...prevFormData, telephone: value }));
   };
 
-  const validate = () => {
-    const newErrors = {};
-    if (!formData.nom) newErrors.nom = 'Le nom est requis';
-    if (!formData.prenom) newErrors.prenom = 'Le prénom est requis';
-    if (!formData.email) {
-      newErrors.email = 'L\'email est requis';
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      newErrors.email = 'L\'email est invalide';
-    }
-    if (!formData.address) newErrors.address = 'L\'adresse est requise';
-    if (!formData.telephone) newErrors.telephone = 'Le téléphone est requis';
-    if (!formData.dateOfBirth) newErrors.dateOfBirth = 'La date de naissance est requise';
-    if (!formData.jobTitle) newErrors.jobTitle = 'Le titre du poste est requis';
-    return newErrors;
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const newErrors = validate();
-    if (Object.keys(newErrors).length > 0) {
-      setErrors(newErrors);
-    } else {
-      try {
-        // Appel à l'API avec Axios
-        const response = await axios.post(`http://localhost:4500/api/v1/profiles/create`, formData);
+    console.log('formData:', formData);
+    setLoading(true);
+    try {
+      // Appel à l'API avec Axios
+      const response = await axios.post(`http://localhost:4500/api/v1/profiles/create`, formData);
 
-        if (response.status === 201) {  // 201 Created
-          toast({
-            title: "Inscription réussie.",
-            description: "Vos informations ont été soumises avec succès.",
-            status: "success",
-            duration: 5000,
-            isClosable: true,
-          });
+      if (response.status === 201) {
+        toast.success("Inscription réussie.\nVos informations ont été soumises avec succès.");
 
-          // Réinitialiser le formulaire
-          setFormData({
-            nom: '',
-            prenom: '',
-            email: '',
-            address: '',
-            telephone: '',
-            dateOfBirth: '',
-            jobTitle: '',
-          });
-          setErrors({});
-        }
-      } catch (error) {
-        // Gérer les erreurs de l'API
-        toast({
-          title: "Erreur d'inscription.",
-          description: error.response?.data?.message || "Une erreur s'est produite lors de l'envoi de vos informations.",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
+        // Réinitialiser le formulaire
+        setFormData({
+          userId: '',
+          name: '',
+          surname: '',
+          email: '',
+          location: '',
+          telephone: '',
+          dateOfBirth: '',
+          jobTitle: '',
+          language: ''
         });
       }
+    } catch (error) {
+      toast.error("Une erreur s'est produite lors de l'inscription. Veuillez réessayer.");
+      console.log(error);
+    } finally {
+      setLoading(false); // Arrête le chargement
     }
+
   };
 
   return (
@@ -122,31 +95,42 @@ const SignupForm = () => {
         <Box>
           <form onSubmit={handleSubmit}>
             <Stack spacing={5}>
-              <FormControl isInvalid={errors.nom}>
-                <FormLabel htmlFor="nom">Nom</FormLabel>
+              <FormControl >
+                <FormLabel htmlFor="userId">Username</FormLabel>
                 <Input
                   type="text"
-                  id="nom"
-                  name="nom"
-                  value={formData.nom}
+                  id="userId"
+                  name="userId"
+                  value={formData.userId}
                   onChange={handleChange}
+                  required
                 />
-                {errors.nom && <Box color="red.500" mt={2}>{errors.nom}</Box>}
               </FormControl>
-
-              <FormControl isInvalid={errors.prenom}>
-                <FormLabel htmlFor="prenom">Prénom</FormLabel>
+              <FormControl >
+                <FormLabel htmlFor="name">Name</FormLabel>
                 <Input
                   type="text"
-                  id="prenom"
-                  name="prenom"
-                  value={formData.prenom}
+                  id="name"
+                  name="name"
+                  value={formData.name}
                   onChange={handleChange}
                 />
-                {errors.prenom && <Box color="red.500" mt={2}>{errors.prenom}</Box>}
+
               </FormControl>
 
-              <FormControl isInvalid={errors.email}>
+              <FormControl >
+                <FormLabel htmlFor="surname">Surname</FormLabel>
+                <Input
+                  type="text"
+                  id="surname"
+                  name="surname"
+                  value={formData.surname}
+                  onChange={handleChange}
+                />
+
+              </FormControl>
+
+              <FormControl >
                 <FormLabel htmlFor="email">Email</FormLabel>
                 <Input
                   type="email"
@@ -155,34 +139,36 @@ const SignupForm = () => {
                   value={formData.email}
                   onChange={handleChange}
                 />
-                {errors.email && <Box color="red.500" mt={2}>{errors.email}</Box>}
+
               </FormControl>
 
-              <FormControl isInvalid={errors.address}>
-                <FormLabel htmlFor="address">Adresse</FormLabel>
+              <FormControl >
+                <FormLabel htmlFor="location">Location</FormLabel>
                 <Input
                   type="text"
-                  id="address"
-                  name="address"
-                  value={formData.address}
+                  id="location"
+                  name="location"
+                  value={formData.location}
                   onChange={handleChange}
                 />
-                {errors.address && <Box color="red.500" mt={2}>{errors.address}</Box>}
+
               </FormControl>
 
-              <FormControl isInvalid={errors.telephone}>
-                <FormLabel htmlFor="telephone">Téléphone</FormLabel>
+              <FormControl>
+                <FormLabel htmlFor="telephone">Phone number</FormLabel>
                 <PhoneInput
                   country={'cm'} // Default country code
                   value={formData.telephone}
                   onChange={handlePhoneChange}
                   inputStyle={{ width: '100%' }}
+                  containerStyle={{ textAlign: 'left' }}
+                  dropdownStyle={{ textAlign: 'left' }}
+
                 />
-                {errors.telephone && <Box color="red.500" mt={2}>{errors.telephone}</Box>}
               </FormControl>
 
-              <FormControl isInvalid={errors.dateOfBirth}>
-                <FormLabel htmlFor="dateOfBirth">Date de Naissance</FormLabel>
+              <FormControl >
+                <FormLabel htmlFor="dateOfBirth">Date of birth</FormLabel>
                 <Input
                   type="date"
                   id="dateOfBirth"
@@ -190,11 +176,10 @@ const SignupForm = () => {
                   value={formData.dateOfBirth}
                   onChange={handleChange}
                 />
-                {errors.dateOfBirth && <Box color="red.500" mt={2}>{errors.dateOfBirth}</Box>}
               </FormControl>
 
-              <FormControl isInvalid={errors.jobTitle}>
-                <FormLabel htmlFor="jobTitle">Titre du poste</FormLabel>
+              <FormControl >
+                <FormLabel htmlFor="jobTitle">Job title</FormLabel>
                 <Input
                   type="text"
                   id="jobTitle"
@@ -202,11 +187,21 @@ const SignupForm = () => {
                   value={formData.jobTitle}
                   onChange={handleChange}
                 />
-                {errors.jobTitle && <Box color="red.500" mt={2}>{errors.jobTitle}</Box>}
               </FormControl>
 
-              <Button type="submit" colorScheme="teal" size="md" mt={4}>
-                S'inscrire
+              <FormControl>
+                <FormLabel htmlFor="language">Language</FormLabel>
+                <Input
+                  type="text"
+                  id="language"
+                  name="language"
+                  value={formData.language}
+                  onChange={handleChange}
+                />
+              </FormControl>
+
+              <Button type="submit" colorScheme="teal" size="md" mt={4} isLoading={loading}>
+                Register
               </Button>
             </Stack>
           </form>
